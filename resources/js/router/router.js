f55a8c5337f15531db1e6bed257cba5a0808967a
Vladimir Semerenko <semerenko.vladim@gmail.com>
2021-04-27 12:07:18 +0300

    Init dev branch

---
+++ resources/js/router/router.js
@@ -0,0 +1,10 @@
+import routes from './routes'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
+})
+
+export default router;
