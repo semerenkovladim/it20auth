f5e1c814aea3a5a80bacc0a1b2782f679e0eaf44
semerenkovladim <66204747+semerenkovladim@users.noreply.github.com>
2021-04-29 00:46:27 +0300

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,40 +8,26 @@
 require('./bootstrap');
 
 window.Vue = require('vue').default;
-<<<<<<< HEAD
 import router from "./router/router";
 import store from "./vuex/store";
 import Vuex from 'vuex'
 import TheLayout from './components/TheLayout'
-=======
 import Vue from 'vue'
-//
-// import Vuex from "vuex";
-//
-// Vue.use(Vuex)
 import router from "./router/router";
 import store from "./vuex/store";
 import TheLayout from "./components/TheLayout";
->>>>>>> 876872aa19569a91030d2b8fe5d3681f6454b5be
 
 
-// Vue.use(Vuex)
+Vue.use(Vuex)
 //
-// const storage = new Vuex.Store({
-//     store
-// })
+const storage = new Vuex.Store({
+    store
+})
 
 const app = new Vue({
     el: '#app',
     router,
-<<<<<<< HEAD
-    storage,
-    render: h => h(TheLayout)
-});
-=======
     store,
     render: h => h(TheLayout)
 })
-
->>>>>>> 876872aa19569a91030d2b8fe5d3681f6454b5be
 
