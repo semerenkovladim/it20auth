0556d4f0c0a871cbbfd75583d301658e9ae7bae3
AlienNT <33194513+AlienNT@users.noreply.github.com>
2021-05-04 19:48:17 +0300

    обновлен компонент

--- resources/js/components/UsersManagement/components/UsersManagement.vue
+++ resources/js/components/UsersManagement/components/UsersManagement.vue
@@ -2,9 +2,6 @@
     <main class="users_management">
         <user-management-settings></user-management-settings>
         <user-management-confirm></user-management-confirm>
-        <!--        <div class="temp_header">-->
-        <!--            <div class="temp_nav"></div>-->
-        <!--        </div>-->
         <div class="container-fluid users_management__container">
             <div class="wrapper col">
                 <placeholder :text="placeholderText"
@@ -13,15 +10,12 @@
                 <div class="row users_management__row users_management__wrapper">
                     <div class="col-md-3 users_management__col">
                         <div class="users_management__add_user">
-                            <button class="add_user__btn" type="button" @click="$router.push('/users-management/new-user')">Добавить пользователя</button>
+                            <button class="add_user__btn" type="button"
+                                    @click="$router.push('/users-management/new-user')">Добавить пользователя
+                            </button>
                         </div>
                         <div class="users_management__users_filter">
-                            <ul class="row users_management__filter_list">
-                                <li class="col-12 users_management__filter_item filter-active">Все пользователи</li>
-                                <li class="col-12 users_management__filter_item">Отдел сайтов</li>
-                                <li class="col-12 users_management__filter_item">Отдел разработки проектов</li>
-                                <li class="col-12 users_management__filter_item">Отдел дизайна</li>
-                            </ul>
+                            <department-list :data="departments"></department-list>
                         </div>
                     </div>
                     <div class="col-md-9 users_management__col users_management__list">
@@ -69,42 +63,10 @@
                                 </form>
                             </div>
                         </div>
-                        <div class="wrapper"
-                             v-if="users.length">
-                            <div class="users_management__sort">
-                                <ul class="row users_management__sort_list">
-                                    <li class="col-1 sort_item"></li>
-                                    <li class="col-2 sort_item"><span>Фамилия</span></li>
-                                    <li class="col-2 sort_item"><span>Имя</span></li>
-                                    <li class="col-2 sort_item"><span>Отчество</span></li>
-                                    <li class="col-3 sort_item"><span>E-mail</span></li>
-                                    <li class="col-2 sort_item"><span>Должность</span></li>
-                                </ul>
-                            </div>
-                            <ul class="users_management__user_list">
-                                <li class="user"
-                                    v-for="user in users"
-                                    :key="user.id">
-                                    <ul class="row user__info">
-                                        <li class="col-1 user_info__item user_checkbox">
-                                            <label>
-                                                <input type="checkbox"
-                                                       class="user_check input_checkbox"
-                                                       :checked="checkAll"
-                                                       @click="toggleCheck">
-                                            </label>
-                                        </li>
-                                        <li class="col-2 user_info__item">{{ user.lastName }}</li>
-                                        <li class="col-2 user_info__item">{{ user.firstName }}</li>
-                                        <li class="col-2 user_info__item">{{ user.patronymic }}</li>
-                                        <li class="col-3 user_info__item">
-                                            <a :href="'mailto:' + user.email">{{ user.email }}</a>
-                                        </li>
-                                        <li class="col-2 user_info__item">{{ user.position }}</li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
+                        <users-management-list
+                            v-if="users.length"
+                            :data="UM_USERS.data">
+                        </users-management-list>
                         <div class="not_found" v-else>Пользователи не найдены</div>
                     </div>
                 </div>
@@ -112,17 +74,9 @@
                      v-if="users.length">
                     <div class="col-md-3"></div>
                     <div class="col-md-9">
-                        <div class="row users_management__paginator">
-                            <nav aria-label="Page navigation">
-                                <ul class="paginator">
-                                    <li class="paginator_item paginator__prev"></li>
-                                    <li class="paginator_item">1</li>
-                                    <li class="paginator_item paginator__current_page">2</li>
-                                    <li class="paginator_item">3</li>
-                                    <li class="paginator_item">4</li>
-                                    <li class="paginator_item paginator__next"></li>
-                                </ul>
-                            </nav>
+                        <div class="row paginator_row">
+                            <paginator :data="UM_USERS"
+                                       @toPage="getUsers"></paginator>
                         </div>
                     </div>
                 </div>
@@ -135,14 +89,20 @@
 import Placeholder from "../../placeholder/Placeholder";
 import UserManagementConfirm from "./UserManagementConfirm";
 import UserManagementSettings from "./UserManagementSettings";
-import {mapActions} from "vuex";
+import UsersManagementList from "./UsersManagementList";
+import DepartmentList from "./DepartmentList";
+import Paginator from "../../paginator/Paginator";
+import {mapActions, mapGetters} from "vuex";
 
 export default {
     name: "UsersManagement",
     components: {
         'placeholder': Placeholder,
+        'paginator': Paginator,
         'user-management-settings': UserManagementSettings,
-        'user-management-confirm': UserManagementConfirm
+        'user-management-confirm': UserManagementConfirm,
+        'users-management-list': UsersManagementList,
+        'department-list': DepartmentList
     },
     data() {
         return {
@@ -158,6 +118,28 @@ export default {
                     id: 2,
                     name: 'Пользователи',
                     href: '#'
+                },
+            ],
+            departments: [
+                {
+                    id: 1,
+                    name: 'Все пользователи',
+                    status: true
+                },
+                {
+                    id: 2,
+                    name: 'Отдел сайтов',
+                    status: false
+                },
+                {
+                    id: 3,
+                    name: 'Отдел разработки проектов',
+                    status: false
+                },
+                {
+                    id: 4,
+                    name: 'Отдел дизайна',
+                    status: false
                 },
             ],
             users: [
@@ -211,13 +193,16 @@ export default {
                 }
             ],
             checkAll: false,
-            checkStatus: false
+            checkStatus: false,
+            paginator: ''
         }
     },
     methods: {
         ...mapActions([
             'changeUMSettingStatus',
-            'changeUMConfirmStatus'
+            'changeUMConfirmStatus',
+            'getUMAllUsers'
+
         ]),
         toggleCheckAll() {
             this.checkAll = !this.checkAll
@@ -236,7 +221,19 @@ export default {
         },
         removeUser() {
             this.changeUMConfirmStatus()
+        },
+        getUsers(data) {
+            console.log('getUsers', data)
+            this.getUMAllUsers(data)
         }
+    },
+    computed: {
+        ...mapGetters([
+            'UM_USERS'
+        ])
+    },
+    created() {
+        this.getUMAllUsers()
     }
 }
 </script>
@@ -249,6 +246,7 @@ export default {
     transition: 0.2s ease;
     font-style: normal;
 }
+
 
 .search__row {
     align-items: center;
@@ -292,60 +290,6 @@ export default {
     }
 }
 
-.users_management__sort .sort_item, .users_management__user_list .user__info {
-    color: $userManagementUserColor;
-    border-bottom: 2px solid #F5F5F5;
-    font-style: normal;
-    font-weight: 500;
-}
-
-.users_management__list {
-    display: flex;
-    flex-direction: column;
-
-    .wrapper {
-        padding: 0 15px;
-        margin-right: -15px;
-        margin-left: -15px;
-        overflow-x: auto;
-        margin-bottom: 4px;
-        border-left: 2px solid #F5F5F5;
-
-        > * {
-            min-width: 800px;
-        }
-    }
-
-    .not_found {
-        text-align: center;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 100%;
-        width: 100%;
-        font-weight: 600;
-        font-size: 18px;
-        color: $lightColor;
-    }
-
-    .search_user {
-        padding: 0 15px;
-        flex: 1 1 66%;
-
-    }
-
-    .edit__actions_list {
-        justify-content: space-between;
-        padding: 0 15px;
-        flex: 1 1 10%;
-
-        .row {
-            padding-right: 10px;
-            justify-content: space-between;
-            flex-wrap: nowrap;
-        }
-    }
-}
 
 .users_management__wrapper {
     padding-top: 10px;
@@ -356,96 +300,6 @@ export default {
     margin-bottom: 5px;
 }
 
-.users_management__user_list {
-    min-height: 55px;
-
-    .user {
-        &:last-child {
-            border-bottom: none;
-
-            .user__info {
-                border-bottom: none;
-            }
-        }
-    }
-
-    .user__info {
-        font-size: $userManagementUserFz;
-        min-height: 55px;
-
-    }
-
-    .user_info__item {
-        display: flex;
-        align-items: center;
-        font-weight: 600;
-        word-break: break-all;
-
-        a {
-            color: $lightColor;
-        }
-    }
-}
-
-.users_management__sort {
-    .users_management__sort_list {
-        border-top: 2px solid #F5F5F5;
-        min-height: 55px;
-    }
-
-    .sort_item {
-        font-size: 13px;
-        color: $darkColor;
-        font-weight: 600;
-        display: flex;
-        align-items: center;
-
-        span {
-            min-height: 26px;
-            align-items: center;
-            display: flex;
-            padding-right: 33px;
-            background: url("../../../../images/icons/arrow_down.svg") no-repeat top right / contain;
-
-        }
-
-        &:first-child {
-            span {
-                background: none;
-            }
-        }
-    }
-
-}
-
-.users_management__filter_item {
-    border-bottom: 2px solid #F5F5F5;
-    min-height: 55px;
-    align-items: center;
-    display: flex;
-    cursor: pointer;
-    border-left: 2px solid transparent;
-    font-weight: 600;
-    font-size: 13px;
-    color: $lightColor;
-
-    &:first-child {
-        border-top: 2px solid #F5F5F5;
-    }
-
-
-    &:hover {
-        background: $userManagementFilterActiveBg;
-        border-left: 2px solid $designColorOne;
-        color: $darkColor;
-    }
-}
-
-.filter-active {
-    background: $userManagementFilterActiveBg;
-    border-left: 2px solid $designColorOne;
-    color: $darkColor;
-}
 
 .users_management__add_user, .users_management__edit {
     min-height: 88px;
@@ -523,79 +377,9 @@ export default {
     background: url("../../../../images/icons/setting_img.png") no-repeat center / contain;
 }
 
-.users_management__paginator {
-    background: #FFFFFF;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.05);
-    border-radius: 6px;
-
-    nav {
-        width: 100%;
-    }
-
-    .paginator {
-        display: flex;
-        height: 60px;
-    }
-
-    .paginator_item {
-        flex: 1 1;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-weight: bold;
-        font-size: 14px;
-        color: $darkColor;
-        width: 100%;
-        max-width: 68px;
-        cursor: pointer;
-        border-right: 2px solid #F5F5F5;
-
-        &:last-child {
-            border-right: none;
-        }
-
-        &:hover {
-            background-color: #FAFAFA;
-        }
-
-    }
-
-    .paginator__prev {
-        background: url("../../../../images/icons/arow-left.png") no-repeat center / 36px 30px;
-    }
-
-    .paginator__next {
-        background: url("../../../../images/icons/arrow-rigth.png") no-repeat center / 36px 30px;
-    }
-
-    .paginator__current_page {
-        background: #FAFAFA;
-    }
+.paginator_row {
+    flex: 1 1 100%
 }
-
-.temp_header, .temp_nav {
-    background: #FFFFFF;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 99;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.05);
-}
-
-//
-//.temp_header {
-//    height: $headerHeight;
-//    width: 100%;
-//
-//}
-//
-//.temp_nav {
-//    height: 100vh;
-//    width: $navWidth;
-//    @media all and (max-width: $breakpoint) {
-//        display: none;
-//    }
-//}
 
 .edit__action_checkbox {
     flex: none;
