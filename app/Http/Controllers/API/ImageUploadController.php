a9012ef8a52e20335b00554792ca1409c9027c83
AlienNT <33194513+AlienNT@users.noreply.github.com>
2021-05-06 01:33:31 +0300

    обновлен контроллер загрузки изобрадений

--- app/Http/Controllers/API/ImageUploadController.php
+++ app/Http/Controllers/API/ImageUploadController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Validator;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class ImageUploadController extends Controller
